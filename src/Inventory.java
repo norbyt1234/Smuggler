7352c0305bb18c7edf193c4deddd14f0e088bfc4
Norby <tabaro2012@yahoo.ca>
2023-06-30 15:46:53 -0400

    Complete Item and inventory

---
+++ src/Inventory.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inventory {
+    private List<Item> items;
+
+    //Inventory constructor
+    public Inventory(){
+        items = new ArrayList<>();
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void addItem(Item item){  //Method to add items in our inventory
+        items.add(item);
+    }
+    public  void removeItem(Item item){  //Method to remove items in our inventory
+        items.remove(item);
+    }
+
+    //Method to get the value of our inventory
+    public double getInventoryValue (){
+        double inventoryValue = 0.0;
+        for (Item item:items) {
+            inventoryValue += item.getBuyingPrice();
+        }
+        return inventoryValue;
+    }
+    public Item searchItemByID(int ID){
+        for(Item item : items ){
+            if (item.getItemID() == ID){
+                return item;
+            }
+        }
+        return null;
+    }
+
+
+}
---
+++ src/Item.java
@@ -0,0 +1,52 @@
+public class Item {
+    private String itemName;
+    private int itemID;
+    private static int nextID = 0;
+    private double buyingPrice;
+    private double sellingPrice;
+    private int quantity;
+
+    public Item(String itemName, int quantity) {
+        nextID++;
+        this.itemName = itemName;
+        this.itemID = nextID;
+        this.quantity = quantity;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getItemID() {
+        return itemID;
+    }
+
+
+    public double getBuyingPrice() {
+        return buyingPrice;
+    }
+
+    public void setBuyingPrice(double buyingPrice) {
+        this.buyingPrice = buyingPrice;
+    }
+
+    public double getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public void setSellingPrice(double sellingPrice) {
+        this.sellingPrice = sellingPrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
